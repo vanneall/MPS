5fa74f28e4bfc12cba23a956d01d70eeeb4472fe
Ivan Demin <103442987+vanneall@users.noreply.github.com>
2023-03-22 08:57:13 +0300

    Конечный main

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-import topsis
-import saw
 import edas
+import saw
+import topsis
 
-
+company = ["Spotify", "Яндекс.музыка", "ВК музыка", "Зайцев.Нет", "Deezer", "SoundCloud", "Apple Music"]
 criteria = ["Совместимость", "Юзабилити", "Надежность", "Защищенность", "Сопровождаемость", "Мобильность", "Стоимость"]
-
-criteriaWeight = [0.1, 0.2, 0.1, 0.1, 0.1, 0.2, 0.2]
-
-suppliers = ["Spotify", "Яндекс музыка", "ВК музыка", "Зайцев.нет", "Deezer", "SoundCloud", "Apple Music"]
 
 matrix = [
     [0.8, 0.7, 0.6, 0.6, 0.8, 0.9, 0.6],  # Совместимость
@@ -16,32 +12,22 @@ matrix = [
     [0.8, 0.69, 0.73, 0.4, 0.75, 0.8, 0.76],  # Защищенность
     [0.98, 0.89, 0.76, 0.64, 0.76, 0.78, 0.6],  # Сопровождаемость
     [0.9, 0.9, 0.7, 0.7, 0.9, 0.9, 0.5],  # Мобильность
-    [0.169, 0.5, 0.159, 0.169, 0.169, 0.169, 0.169]  # Стоимость
+    [0.169, 0.299, 0.159, 0.169, 0.169, 0.369, 0.169]  # Стоимость для SAW и TOPSIS не менять, для EDAS вычитать из 1
 ]
 
-choice = int(input("1. TOPSIS\n2. SAW\n3. EDAS\n>>> "))
-if choice == 1:
-    matrixWithCriteriaWeight = topsis.createMatrixWithCriteriaWeight(criteria, suppliers, matrix, criteriaWeight)
-    positive, negative = topsis.findPositiveAndNegativeDecision(matrixWithCriteriaWeight, criteria)
-    plus, minus = topsis.findDistance(positive, negative, matrixWithCriteriaWeight, suppliers, criteria)
-    P = topsis.getResult(plus, minus)
-    for i in suppliers:
-        print(i, end=" | ")
-    print()
-    for i in P:
-        print(f'{i:.5}', end=" | ")
-elif choice == 2:
-    matrixWithCriteriaWeight = saw.createMatrixWithCriteriaWeight(criteria, suppliers, matrix, criteriaWeight)
-    P = saw.getRating(criteria, suppliers, matrixWithCriteriaWeight)
-    for i in suppliers:
-        print(i, end=" | ")
-    print()
-    for i in P:
-        print(f'{i:.5}', end=" | ")
-elif choice == 3:
-    avrgScore = edas.getAvarageScoreFromCriteria(matrix)
-    newMatrix = edas.getPositiveMatrix(matrix, avrgScore, criteriaWeight)
-    for i in newMatrix:
-        for j in i:
-            print(j, end=" ")
-        print()
+weight_matrix = [0.1, 0.2, 0.1, 0.2, 0.1, 0.1, 0.2]
+
+print(company)
+#SAW
+matrix_with_criteria_weight = saw.createMatrixWithCriteriaWeight(criteria, company, matrix, weight_matrix)
+print(saw.getRating(matrix_with_criteria_weight, company, matrix))
+
+#TOPSIS
+matrix_with_criteria_weight = topsis.createMatrixWithCriteriaWeight(criteria, company, matrix, weight_matrix)
+distance = topsis.findPositiveAndNegativeDecision(matrix_with_criteria_weight, criteria)
+pos, neg = topsis.findPositiveAndNegativeDecision(matrix_with_criteria_weight, criteria)
+print(topsis.getResult(pos, neg))
+
+#EDAS
+avg = edas.getAvarageScoreFromCriteria(matrix)
+print(edas.getResult(matrix, avg, weight_matrix))
